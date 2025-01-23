2425bc700b4b15a4514d193b9ca4716de4f3d0a4
Jakub Piskiewicz <jpiskiewicz@svevn.com>
2025-01-23 13:09:52 +0100

    Add copy code button to fenced code blocks

--- lib/kramdown/converter/html.rb
+++ lib/kramdown/converter/html.rb
@@ -123,7 +123,9 @@ module Kramdown
           end
           code_attr = {}
           code_attr['class'] = "language-#{lang}" if lang
-          "#{' '*indent}<pre#{html_attributes(attr)}><code#{html_attributes(code_attr)}>#{result}\n</code></pre>\n"
+          "#{' ' * indent}<pre#{html_attributes(attr)}>" \
+          "<button onclick=\"({ target }) => navigator.clipboard.writeText(target.children.querySelector('code').innerText)\">copy</button>" \
+            "<code#{html_attributes(code_attr)}>#{result}\n</code></pre>\n"
         end
       end
 
